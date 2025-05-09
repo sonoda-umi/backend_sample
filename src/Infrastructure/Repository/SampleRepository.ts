13f2a403607a64d851e0167a27d7c76a19eaed16
Likun Liu <liu-l@kadokawa.jp>
2025-05-10 02:20:11 +0900

    add contents to gitignore, and use dynamic port allocating

--- src/Infrastructure/Repository/SampleRepository.ts
+++ src/Infrastructure/Repository/SampleRepository.ts
@@ -1,13 +1,16 @@
 import { Sample } from '@src/Domain/Sample'
-import { Prisma, PrismaClient } from '@prisma/client'
+// import { Prisma, PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
 export class SampleRepository {
   async get(): Promise<Sample> {
     // 本来DBから取ったりなんだり...
     // 今回はSampleの為、割愛
-    const sampleData = await prisma.Sample.findMany()
-    return sampleData
+    // const sampleData = await prisma.Sample.findMany()
+    const sampleData = new Sample();
+    sampleData.id = 1;
+    sampleData.name = "sample"
+    return sampleData;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Server } from './Presentation/Server'
 
 async function main() {
+  const port: Number = parseInt(<string>process.env.APP_PORT, 10) || 3000
   const server = Server.create()
-  server.listen(3000)
+  server.listen(port)
   server.on('listening', () => {})
 }
 
